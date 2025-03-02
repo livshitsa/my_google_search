7c2b58e58dbb76fa037daf4d02b7e0f5800cdf0f
aleksl <alekl>
2025-03-02 08:46:27 -0500

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+<<<<<<< HEAD
 # my_google_search
+=======
+**create virtual environment**
+- uv venv --seed -p 3.11
+- activate virtual enviroment (on mac or linux: source .venv/bin/activate)
+- install dependancies: uv pip install -r ./requirements.txt
+
+**mcp config section**
+```
+"Google Search": {
+    "command": "your_path_to_uv_executable",
+    "args": [
+    "--directory",
+    "/location_where_you_placed_python_file/",
+    "run",
+    "search_server.py"
+    ]
+}
+```
+>>>>>>> 0868bbf (init commit)
---
+++ search_server.py
@@ -0,0 +1,43 @@
+from googlesearch import search as google_search
+import json
+from typing import Any, Dict, List
+from mcp.server.fastmcp import FastMCP
+import json
+
+# Initialize the FastMCP server
+mcp = FastMCP("Google Search")
+
+@mcp.tool()
+def search(query: str, max_results: int = 5) -> str:
+    """Search the web using Google.
+    
+    Args:
+        query: The search query
+        max_results: Maximum number of results to return (default: 5)
+    
+    Returns:
+        JSON containing title , url , description
+    """
+    try:
+        results = list(google_search(query, num_results=max_results, lang='en', advanced=True))
+
+        # Collect the search results
+        res: List[Dict[str, str]] = []
+        for result in results:
+            res.append(
+                {
+                    "title": result.title,
+                    "url": result.url,
+                    "description": result.description,
+                }
+            )
+
+        return json.dumps(res, indent=2)
+
+    except Exception as e:
+        print(e)
+        return f"Error performing search: {str(e)}"
+
+if __name__ == "__main__":
+    # Run the server using stdio transport
+    mcp.run(transport="stdio")
